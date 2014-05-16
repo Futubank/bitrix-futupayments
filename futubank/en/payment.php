2d76ae2eea7cdcd6a7ef218e2975ef9502ca02ed
Maxim Oransky <maxim.oransky@gmail.com>
2014-05-16 19:12:39 +0400

    английские переводы

--- futubank/en/payment.php
+++ futubank/en/payment.php
@@ -1,4 +1,44 @@
 <?
 global $MESS;
 
+$MESS['SPCP_DTITLE'] = 'Futubank.com';
+$MESS['SPCP_DDESCR'] = ''; 
+
+$MESS['AMOUNT'] = 'Amount';
+$MESS['AMOUNT_DESCR'] = '';
+
+$MESS['CLIENT_EMAIL'] = 'Client e-mail';
+$MESS['CLIENT_EMAIL_DESCR'] = '';
+
+$MESS['CLIENT_NAME'] = 'Client name';
+$MESS['CLIENT_NAME_DESCR'] = '';
+
+$MESS['CLIENT_PHONE'] = 'Clinent phone';
+$MESS['CLIENT_PHONE_DESCR'] = '';
+
+$MESS['CURRENCY'] = 'Currency';
+$MESS['CURRENCY_DESCR'] = 'RUB only';
+
+$MESS['FAIL_URL'] = 'Fail url';
+$MESS['FAIL_URL_DESC'] = 'Default: https://secure.futubank.com/fail/';
+
+$MESS['SUCCESS_URL'] = 'Success url';
+$MESS['SUCCESS_URL_DESC'] = 'Default https://secure.futubank.com/success/';
+
+$MESS['MERCHANT_ID'] = 'MERCHANT_ID';
+$MESS['MERCHANT_ID_DESCR'] = '';
+
+$MESS['SECRET_KEY'] = 'SECRET_KEY';
+$MESS['SECRET_KEY_DESCR'] = '';
+
+$MESS['PYM_TITLE'] = 'Pay order with <b>Futubank.com</b>';
+$MESS['PYM_TO_PAY'] = 'Amount:';
+$MESS['PYM_BUTTON'] = 'Pay';
+
+$MESS['PYM_CHANGE_STATUS_PAY'] = 'Change pay status';
+$MESS['PYM_CHANGE_STATUS_PAY_DESC'] = 'Y / N ';
+
+$MESS['PYM_TEST'] = 'Test mode';
+$MESS['PYM_TEST_DESC'] = 'Y / N';
+
 ?>
