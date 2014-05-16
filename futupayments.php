4d60f245f7200de198b93350e6be0a51c9c84fd7
Maxim Oransky <maxim.oransky@gmail.com>
2014-05-16 19:12:09 +0400

    обработчик callbackов

---
+++ futupayments.php
@@ -0,0 +1,17 @@
+<?
+ob_start();
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+ob_end_clean();
+ob_end_flush();
+
+$bits = split(':', $_POST['meta']);
+
+$APPLICATION->IncludeComponent(
+	'bitrix:sale.order.payment.receive',
+	'',
+	Array(
+		'PAY_SYSTEM_ID' => $bits[0],
+		'PERSON_TYPE_ID' => $bits[1],
+	)
+);
+?>
